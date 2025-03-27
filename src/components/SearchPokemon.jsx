ad1d28f6ae1eb1281b70329962e24371c1a091c4
anke180 <153827509+anke180@users.noreply.github.com>
2025-03-27 12:25:04 +0100

    Start
    I made a small start with the website. This is also a test.

---
+++ src/components/SearchPokemon.jsx
@@ -0,0 +1,12 @@
+import "./css/index.css";
+import {useState} from "react";
+
+function SearchPokemon() {
+    return (
+        <div class="SearchBar">
+            <h1>Nav</h1>
+        </div>
+    );
+}
+
+export default SearchPokemon;
